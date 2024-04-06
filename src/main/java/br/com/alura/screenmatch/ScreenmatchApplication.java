d02426a30e6a5dd7e2bda691d8ed82e1f0ab7c5c
Maciel <pedrohenrique_maciel@hotmail.com>
2024-04-06 18:32:04 -0300

    start integrate db sql with jpa

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.principal.Principal;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+  @Autowired
+  private SerieRepository serieRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
-		principal.exibeMenu();
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ScreenmatchApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    Principal principal = new Principal(serieRepository);
+    principal.exibeMenu();
+  }
 }
