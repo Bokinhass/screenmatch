a8138360cfd2edacde5506ad2296edbd65d94fe2
Maciel <pedrohenrique_maciel@hotmail.com>
2024-04-14 19:34:38 -0300

    create services and controller por integrate api with front end

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,25 +1,17 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.principal.Principal;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication {
 
   @Autowired
   private SerieRepository serieRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(ScreenmatchApplication.class, args);
-  }
-
-  @Override
-  public void run(String... args) throws Exception {
-    Principal principal = new Principal(serieRepository);
-    principal.exibeMenu();
   }
 }
